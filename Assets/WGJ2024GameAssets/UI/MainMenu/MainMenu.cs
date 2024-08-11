259453da95a9480a597491e86aa0bd3637962b74
capaths <pcampaa@gmail.com>
2024-08-11 18:45:29 -0400

    feat: add main menu and pause menu

---
+++ Assets/WGJ2024GameAssets/UI/MainMenu/MainMenu.cs
@@ -0,0 +1,137 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
+using UnityEngine.UIElements;
+
+public class MainMenu : MonoBehaviour
+{
+    [Header("Scenes")]
+    public string gameSceneName = "Scenes/SampleScene";
+    
+    [Header("Windows")]
+    public string creditsWindowName = "CreditsWindow";
+
+    [Header("Buttons - Main Menu")]
+    public string startButtonSelector = "StartButton";
+    public string creditsButtonSelector = "CreditsButton";
+    public string exitButtonSelector = "ExitButton";
+
+    [Header("Buttons - Credits")]
+    public string creditsBackButtonSelector = "CreditsBackButton";
+
+    [CanBeNull] private Button _playButton;
+    [CanBeNull] private Button _creditsButton;
+    [CanBeNull] private Button _creditsBackButton;
+    [CanBeNull] private Button _exitButton;
+    [CanBeNull] private VisualElement _creditsWindow;
+
+    void Awake()
+    {
+        UIDocument uiDocument;
+        try
+        {
+             uiDocument = GetComponent<UIDocument>();
+        }
+        catch (NullReferenceException e)
+        {
+            Debug.LogError("Error: UIDocument not found in MainMenu. Skipping button setup.");
+            return;
+        }
+
+        _playButton = uiDocument.rootVisualElement.Q<Button>(startButtonSelector);
+        _creditsButton = uiDocument.rootVisualElement.Q<Button>(creditsButtonSelector);
+        _creditsBackButton = uiDocument.rootVisualElement.Q<Button>(creditsBackButtonSelector);
+        _exitButton = uiDocument.rootVisualElement.Q<Button>(exitButtonSelector);
+
+        if (_playButton != null)
+        {
+            _playButton.clicked += StartGame;
+        }
+        else
+        {
+            Debug.LogWarning("Error: StartButton not found in MainMenu. Start button not setup.");
+        }
+
+        if (_creditsButton != null)
+        {
+            _creditsButton.clicked += ShowCredits;
+        }
+        else
+        {
+            Debug.LogWarning("Error: CreditsButton not found in MainMenu. Credits button not setup.");
+        }
+
+        if (_exitButton != null)
+        {
+            _exitButton.clicked += ExitGame;
+        }
+        else
+        {
+            Debug.LogWarning("Error: ExitButton not found in MainMenu. Exit button not setup.");
+        }
+
+        if (_creditsBackButton != null)
+        {
+            _creditsBackButton.clicked += BackToMainMenu;
+        }
+        else
+        {
+            Debug.LogWarning("Error: CreditsBackButton not found in MainMenu. Credits back button not setup.");
+        }
+
+        _creditsWindow = uiDocument.rootVisualElement.Q<VisualElement>(creditsWindowName);
+    }
+
+    private void StartGame()
+    {
+        SceneManager.LoadScene(gameSceneName);
+    }
+
+    private void ShowCredits()
+    {
+        if (_creditsWindow != null)
+        {
+            _creditsWindow.style.display = DisplayStyle.Flex;
+        }
+    }
+
+    private void BackToMainMenu()
+    {
+        if (_creditsWindow != null)
+        {
+            _creditsWindow.visible = false;
+        }
+    }
+
+    private void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    private void OnDestroy()
+    {
+        if (_playButton != null)
+        {
+            _playButton.clicked -= StartGame;
+        }
+
+        if (_creditsButton != null)
+        {
+            _creditsButton.clicked -= ShowCredits;
+        }
+
+        if (_exitButton != null)
+        {
+            _exitButton.clicked -= ExitGame;
+        }
+
+        if (_creditsBackButton != null)
+        {
+            _creditsBackButton.clicked -= BackToMainMenu;
+        }
+    }
+}
---
+++ Assets/WGJ2024GameAssets/UI/MainMenu/PauseMenu.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
+using UnityEngine.UIElements;
+
+namespace WGJ2024GameAssets.UI.MainMenu
+{
+    public class PauseMenu : MonoBehaviour
+    {
+        [Header("Scenes")]
+        public string menuSceneName = "Scenes/MainMenu";
+
+        [Header("Elements Selectors")]
+        public string pauseWindowSelector = "PauseWindow";
+        public string volumeSliderSelector = "VolumeSlider";
+        public string resumeButtonSelector = "ResumeButton";
+        public string exitButtonSelector = "ExitButton";
+
+        [CanBeNull] private UIDocument _uiDocument;
+
+        [Header("Events")]
+        [CanBeNull] public AudioSource audioSource;
+
+        public static bool IsPaused => Time.timeScale <= 0.0001f;
+
+        [CanBeNull] private Slider _volumeSlider;
+        [CanBeNull] private Button _resumeButton;
+        [CanBeNull] private Button _exitButton;
+        [CanBeNull] private VisualElement _pauseWindow;
+        
+
+        public void Start()
+        {
+            try
+            {
+                _uiDocument = GetComponent<UIDocument>();
+            }
+            catch (NullReferenceException)
+            {
+                Debug.LogError("Error: UIDocument not found in PauseMenu. Skipping button setup.");
+                return;
+            }
+
+            _volumeSlider = _uiDocument?.rootVisualElement.Q<Slider>(volumeSliderSelector);
+            if (_volumeSlider != null)
+            {
+                if (audioSource)
+                {
+                    _volumeSlider.value = audioSource.volume * (_volumeSlider.highValue - _volumeSlider.lowValue) + _volumeSlider.lowValue;
+                }
+                _volumeSlider.RegisterValueChangedCallback(evt =>
+                {
+                    if (!audioSource) return;
+
+                    float volume = (evt.newValue - _volumeSlider.lowValue) / (_volumeSlider.highValue - _volumeSlider.lowValue);
+                    audioSource.volume = volume;
+                });
+            }
+            else
+            {
+                Debug.LogWarning("Error: VolumeSlider not found in PauseMenu. Volume slider not setup.");
+            }
+
+            _resumeButton = _uiDocument?.rootVisualElement.Q<Button>(resumeButtonSelector);
+            if (_resumeButton != null)
+            {
+                _resumeButton.clicked += () => ResumeGame();
+            }
+            else
+            {
+                Debug.LogWarning("Error: ResumeButton not found in PauseMenu. Resume button not setup.");
+            }
+
+            _exitButton = _uiDocument?.rootVisualElement.Q<Button>(exitButtonSelector);
+            if (_exitButton != null)
+            {
+                _exitButton.clicked += () => ExitToMenu();
+            }
+            else
+            {
+                Debug.LogWarning("Error: ExitButton not found in PauseMenu. Exit button not setup.");
+            }
+
+            _pauseWindow = _uiDocument?.rootVisualElement.Q<VisualElement>(pauseWindowSelector);
+            if (_pauseWindow != null)
+            {
+                _pauseWindow.style.display = DisplayStyle.None;
+            }
+            else
+            {
+                Debug.LogWarning("Error: PauseWindow not found in PauseMenu. Pause window not setup.");
+            }
+        }
+
+        private void Update()
+        {
+            switch (IsPaused)
+            {
+                case true when Input.GetKeyUp(KeyCode.Escape):
+                    ResumeGame();
+                    break;
+                case false when Input.GetKeyUp(KeyCode.Escape):
+                    PauseGame();
+                    break;
+            }
+        }
+
+        private void ResumeGame()
+        {
+            Time.timeScale = 1;
+            if (audioSource) audioSource.UnPause();
+
+            if (_pauseWindow != null) _pauseWindow.style.display = DisplayStyle.None;
+        }
+
+        private void PauseGame()
+        {
+            Time.timeScale = 0;
+            if (audioSource) audioSource.Pause();
+
+            if (_pauseWindow != null)
+            {
+                _pauseWindow.style.display = DisplayStyle.Flex;
+            }
+        }
+
+        private void ExitToMenu()
+        {
+            Time.timeScale = 1;
+            SceneManager.LoadScene(menuSceneName);
+        }
+    }
+}
